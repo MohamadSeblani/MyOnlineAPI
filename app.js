f29c4332447f8de0fa2cbd24b6c92e3f73c3721f
MohamadSeblani <mohammad_seblani@live.com>
2023-11-12 22:21:20 +0200

    Enable Cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const PORT = process.env.PORT || 3000 // 3arrafna shi esmo env, hatta ma njbor l app y3mel listing lal api 3a port 3000
 // mensir bas badna nekod files mnezra3 fihon file esmo .env w men3aref fi l port l bdna y3mel listing 3le
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
